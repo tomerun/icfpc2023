2fe637d594db10d2d8ea1f5bf8adccce7d4710c5
tomerun <tomerunmail@gmail.com>
2023-07-09 16:06:17 +0900

    bugfix

--- solver/run.sh
+++ solver/run.sh
@@ -16,8 +16,8 @@ crystal build --release solver.cr
 for (( i = $SEED_START; i < $SEED_END; i++ )); do
 	seed=$(printf "%04d" $i)
 	echo "seed:$seed"
-	TL=60000 ./solver < problem/"$seed".json > result/"$seed".json 2> >(tee -a log.txt >&2)
+	TL=300000 ./solver < problem/"$seed".json > result/"$seed".json 2> >(tee -a log.txt >&2)
 done
 
 aws s3 cp log.txt s3://marathon-tester/$RESULT_PATH/$(printf "%04d" $SEED_START).txt
-aws s3 cp result/*.json s3://marathon-tester/$RESULT_PATH/
+aws s3 cp result s3://marathon-tester/$RESULT_PATH/ --recursive --exclude "*" --include "*.json"
