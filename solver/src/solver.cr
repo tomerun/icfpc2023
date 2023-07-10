d64f5d57da3ce9ce7eb5a4d7f6e685eb0615d033
tomerun <tomerunmail@gmail.com>
2023-07-10 20:10:30 +0900

    opt

--- solver/src/solver.cr
+++ solver/src/solver.cr
@@ -5,11 +5,12 @@ TL              = (ENV["TL"]? || 2000).to_i
 PART            = (ENV["PART"]? || 1).to_i
 INITIAL_TEMP    = (ENV["IT"]? || 100).to_f * 1e-5
 FINAL_TEMP      = (ENV["FT"]? || 10).to_f * 1e-5
-MOVE_DIST       = (ENV["MD"]? || 500).to_f * 0.1
+MOVE_DIST       = (ENV["MD"]? || 1000).to_f * 0.1
 INF             = 1 << 28
 EMPTY           =  -1
 BLOCK_BY_PILLAR =  -2
-ATT_BUCKET      = 100
+ATT_BUCKET_SIZE = 100
+M_BUCKET_SIZE   = 100
 COUNTER         = Counter.new
 STOPWATCH       = StopWatch.new
 RND             = Random.new(2)
@@ -183,6 +184,43 @@ def touch_points(p0, p1, r)
   end
 end
 
+def cross(bottom, left, top, right, p0, p1)
+  return false if p0.y <= bottom && p1.y <= bottom
+  return false if p0.x <= left && p1.x <= left
+  return false if top <= p0.y && top <= p1.y
+  return false if right <= p0.x && right <= p1.x
+  # TODO
+  return true
+end
+
+def rect_dist2(bottom, left, top, right, p)
+  if p.y < bottom
+    if p.x < left
+      return dist2(p.y, p.x, bottom, left)
+    elsif p.x < right
+      return (bottom - p.y) ** 2
+    else
+      return dist2(p.y, p.x, bottom, right)
+    end
+  elsif p.y < top
+    if p.x < left
+      return (left - p.x) ** 2
+    elsif p.x < right
+      return 0.0
+    else
+      return (p.x - right) ** 2
+    end
+  else
+    if p.x < left
+      return dist2(p.y, p.x, top, left)
+    elsif p.x < right
+      return (p.y - top) ** 2
+    else
+      return dist2(p.y, p.x, top, right)
+    end
+  end
+end
+
 class Solver
   @instruments : Array(Int32)
   @attendees : Array(Attendee)
@@ -228,15 +266,24 @@ class Solver
     # blocker_of[i] = [{j, k}] := musician i is blocking sound of musician j to attendee k
     @blocker_of = Array(Set(Tuple(Int32, Int32))).new(@mn) { Set(Tuple(Int32, Int32)).new }
     @raw_score = Array(Float64).new(@mn, 0.0)
-    @att_rows = Array(Array(Int32)).new((room_h / ATT_BUCKET).floor.to_i + 1) { [] of Int32 }
-    @att_cols = Array(Array(Int32)).new((room_w / ATT_BUCKET).floor.to_i + 1) { [] of Int32 }
+    @att_rows = Array(Array(Int32)).new((room_h / ATT_BUCKET_SIZE).floor.to_i + 1) { [] of Int32 }
+    @att_cols = Array(Array(Int32)).new((room_w / ATT_BUCKET_SIZE).floor.to_i + 1) { [] of Int32 }
     @orig_taste = [] of Array(Float64)
     @an.times do |ai|
-      @att_rows[(@attendees[ai].pos.y / ATT_BUCKET).floor.to_i] << ai
-      @att_cols[(@attendees[ai].pos.x / ATT_BUCKET).floor.to_i] << ai
+      @att_rows[(@attendees[ai].pos.y / ATT_BUCKET_SIZE).floor.to_i] << ai
+      @att_cols[(@attendees[ai].pos.x / ATT_BUCKET_SIZE).floor.to_i] << ai
       @orig_taste << @attendees[ai].taste.dup
       @attendees[ai].taste.map! { |v| {v, 0.0}.max }
     end
+    mbh = (stage_h / M_BUCKET_SIZE).floor.to_i + 1
+    mbw = (stage_w / M_BUCKET_SIZE).floor.to_i + 1
+    @m_buckets = Array(Array(Set(Int32))).new(mbh) { Array.new(mbw) { Set(Int32).new } }
+  end
+
+  def mb_pos(pos)
+    r = pos.y - @stage.bottom
+    c = pos.x - @stage.left
+    return {(r / ATT_BUCKET_SIZE).floor.to_i, (c / ATT_BUCKET_SIZE).floor.to_i}
   end
 
   def solve(timelimit)
@@ -249,7 +296,7 @@ class Solver
     cnt_cand_pos = (((@stage.right - @stage.left) / 10).floor.to_i + 1) * (((@stage.top - @stage.bottom) / (5 * (3 ** 0.5))).floor.to_i + 1)
     {(cnt_cand_pos / @mn).ceil.to_i, 10}.min.times { change_types << ChangeType::MOVE }
     if @in != 1
-      100.times { change_types << ChangeType::SWAP }
+      20.times { change_types << ChangeType::SWAP }
     end
     initial_jump_prob = {cnt_cand_pos / @mn, 10.0}.min * 0.09
     jump_prob = initial_jump_prob
@@ -264,6 +311,10 @@ class Solver
     nega_ratio = 0.0
     while true
       # debug("score:#{score} verify_score:#{verify_score(mps)}")
+      # @mn.times do |mi|
+      #   mbi = mb_pos(mps[mi])
+      #   assert(@m_buckets[mbi[0]][mbi[1]].includes?(mi))
+      # end
       # @mn.times do |vmi|
       #   @an.times do |vai|
       #     bi = @blocked_by[vmi][vai]
@@ -313,6 +364,7 @@ class Solver
             end
           end
         end
+        # debug(@m_buckets.map { |row| row.map { |v| v.size } }.flatten.sort)
         # debug("score:#{score} verify_score:#{verify_score(mps)}")
       end
       turn += 1
@@ -368,6 +420,16 @@ class Solver
           STOPWATCH.start("swap_accept")
           score += diff
           mps.swap(mi0, mi1)
+          mbi0 = mb_pos(mps[mi0])
+          mbi1 = mb_pos(mps[mi1])
+          if mbi0 != mbi1
+            assert(@m_buckets[mbi0[0]][mbi0[1]].includes?(mi1))
+            @m_buckets[mbi0[0]][mbi0[1]].delete(mi1)
+            @m_buckets[mbi0[0]][mbi0[1]].add(mi0)
+            assert(@m_buckets[mbi1[0]][mbi1[1]].includes?(mi0))
+            @m_buckets[mbi1[0]][mbi1[1]].delete(mi0)
+            @m_buckets[mbi1[0]][mbi1[1]].add(mi1)
+          end
           @quality[mi0] = new_q0
           @quality[mi1] = new_q1
           @raw_score[mi0] = new_raw0
@@ -519,8 +581,25 @@ class Solver
         STOPWATCH.stop("new_sort")
         STOPWATCH.start("new")
         @blocker_of[mi0].each do |mi, ai|
-          is_blocked = block(mps[mi], np, @attendees[ai].pos, 5.0)
-          is_blocked |= near_mis.any? { |bmi| bmi != mi && block(mps[mi], mps[bmi], @attendees[ai].pos, 5.0) }
+          next if block(mps[mi], np, @attendees[ai].pos, 5.0)
+          is_blocked = false
+          mbi = mb_pos(mps[mi])
+          @m_buckets.size.times do |ri|
+            bb = @stage.bottom + M_BUCKET_SIZE * ri - 5
+            bt = @stage.bottom + M_BUCKET_SIZE * (ri + 1) + 5
+            @m_buckets[0].size.times do |ci|
+              next if @m_buckets[ri][ci].empty?
+              bl = @stage.left + M_BUCKET_SIZE * ci - 5
+              br = @stage.left + M_BUCKET_SIZE * (ci + 1) + 5
+              if cross(bb, bl, bt, br, mps[mi], @attendees[ai].pos)
+                is_blocked = @m_buckets[ri][ci].any? { |bmi| bmi != mi && block(mps[mi], mps[bmi], @attendees[ai].pos, 5.0) }
+              end
+              break if is_blocked
+            end
+            break if is_blocked
+          end
+          next if is_blocked
+          # is_blocked = near_mis.any? { |bmi| bmi != mi && block(mps[mi], mps[bmi], @attendees[ai].pos, 5.0) }
           is_blocked |= @pillars.any? { |pl| block(mps[mi], pl.pos, @attendees[ai].pos, pl.r) }
           if !is_blocked
             d2 = dist2(mps[mi], @attendees[ai].pos)
@@ -567,10 +646,19 @@ class Solver
           score += diff
           @quality = new_quality
           @raw_score = new_raw
+          mbi = mb_pos(np)
+          ombi = mb_pos(mp)
+          if ombi != mbi
+            assert(!@m_buckets[mbi[0]][mbi[1]].includes?(mi0))
+            @m_buckets[mbi[0]][mbi[1]].add(mi0)
+            assert(@m_buckets[ombi[0]][ombi[1]].includes?(mi0))
+            @m_buckets[ombi[0]][ombi[1]].delete(mi0)
+          end
+
           # debug("mi0:#{mi0} score:#{score} verify_score:#{verify_score(mps)} #{change_type}")
 
           # 他の人たちへの影響
-          STOPWATCH.start("after")
+          STOPWATCH.start("after1")
           blocker0 = @blocker_of[mi0].dup
           @blocker_of[mi0].clear
           blocker0.each do |t|
@@ -578,13 +666,29 @@ class Solver
             assert(@blocked_by[mi][ai] == mi0)
             min_dist = 1e10
             block_by = EMPTY
-            @mn.times do |bmi|
-              next if mi == bmi
-              if block(mps[mi], mps[bmi], @attendees[ai].pos, 5.0)
-                d2 = dist2(mps[mi], mps[bmi])
-                if d2 < min_dist
-                  min_dist = d2
-                  block_by = bmi
+            mbi = mb_pos(mps[mi])
+            @m_buckets.size.times do |ri|
+              bb = @stage.bottom + M_BUCKET_SIZE * ri - 5
+              bt = @stage.bottom + M_BUCKET_SIZE * (ri + 1) + 5
+              @m_buckets[0].size.times do |ci|
+                next if @m_buckets[ri][ci].empty?
+                bl = @stage.left + M_BUCKET_SIZE * ci - 5
+                br = @stage.left + M_BUCKET_SIZE * (ci + 1) + 5
+                if min_dist != 1e10 && rect_dist2(bb, bl, bt, br, mps[mi]) > min_dist
+                  COUNTER.add(6)
+                  next
+                end
+                if cross(bb, bl, bt, br, mps[mi], @attendees[ai].pos)
+                  @m_buckets[ri][ci].each do |bmi|
+                    next if mi == bmi
+                    if block(mps[mi], mps[bmi], @attendees[ai].pos, 5.0)
+                      d2 = dist2(mps[mi], mps[bmi])
+                      if d2 < min_dist
+                        min_dist = d2
+                        block_by = bmi
+                      end
+                    end
+                  end
                 end
               end
             end
@@ -605,9 +709,12 @@ class Solver
               @blocker_of[block_by] << {mi, ai}
             end
           end
+          STOPWATCH.stop("after1")
 
+          STOPWATCH.start("after2")
           @mn.times do |mi|
             next if mi == mi0
+            nd2 = dist2(mps[mi], np)
             min_yb, max_yb, min_xb, max_xb = get_att_bucket(np, mps[mi])
             if (max_yb - min_yb + 1) / @att_rows.size < (max_xb - min_xb + 1) / @att_cols.size
               min_yb.upto(max_yb) do |yb|
@@ -620,7 +727,7 @@ class Solver
                     else
                       cd2 = dist2(mps[mi], mps[@blocked_by[mi][ai]])
                     end
-                    if dist2(mps[mi], np) < cd2
+                    if nd2 < cd2
                       if @blocked_by[mi][ai] >= 0
                         assert(@blocker_of[@blocked_by[mi][ai]].includes?({mi, ai}))
                         @blocker_of[@blocked_by[mi][ai]].delete({mi, ai})
@@ -642,7 +749,7 @@ class Solver
                     else
                       cd2 = dist2(mps[mi], mps[@blocked_by[mi][ai]])
                     end
-                    if dist2(mps[mi], np) < cd2
+                    if nd2 < cd2
                       if @blocked_by[mi][ai] >= 0
                         assert(@blocker_of[@blocked_by[mi][ai]].includes?({mi, ai}))
                         @blocker_of[@blocked_by[mi][ai]].delete({mi, ai})
@@ -655,7 +762,7 @@ class Solver
               end
             end
           end
-          STOPWATCH.stop("after")
+          STOPWATCH.stop("after2")
 
           # 移動先からの線のブロック
           STOPWATCH.start("moved")
@@ -709,17 +816,17 @@ class Solver
   def get_att_bucket(mp, mp2)
     if mp.y <= mp2.y
       min_yb = 0
-      max_yb = mp.y + 5 > mp2.y ? @att_rows.size - 1 : ((mp.y + 5) / ATT_BUCKET).floor.to_i
+      max_yb = mp.y + 5 > mp2.y ? @att_rows.size - 1 : ((mp.y + 5) / ATT_BUCKET_SIZE).floor.to_i
     else
       max_yb = @att_rows.size - 1
-      min_yb = mp.y - 5 < mp2.y ? 0 : ((mp.y - 5) / ATT_BUCKET).floor.to_i
+      min_yb = mp.y - 5 < mp2.y ? 0 : ((mp.y - 5) / ATT_BUCKET_SIZE).floor.to_i
     end
     if mp.x <= mp2.x
       min_xb = 0
-      max_xb = mp.x + 5 > mp2.x ? @att_cols.size - 1 : ((mp.x + 5) / ATT_BUCKET).floor.to_i
+      max_xb = mp.x + 5 > mp2.x ? @att_cols.size - 1 : ((mp.x + 5) / ATT_BUCKET_SIZE).floor.to_i
     else
       max_xb = @att_cols.size - 1
-      min_xb = mp.x - 5 < mp2.x ? 0 : ((mp.x - 5) / ATT_BUCKET).floor.to_i
+      min_xb = mp.x - 5 < mp2.x ? 0 : ((mp.x - 5) / ATT_BUCKET_SIZE).floor.to_i
     end
     return {min_yb, max_yb, min_xb, max_xb}
   end
@@ -790,6 +897,8 @@ class Solver
     mps = used_pos_i.map { |pi| cand_pos[pi] }
     @mn.times do |i|
       mp = mps[i]
+      mbi = mb_pos(mp)
+      @m_buckets[mbi[0]][mbi[1]] << i
       inst = @instruments[i]
       angles = rad_sort(mps, i) do |ai, blocker|
         if blocker == EMPTY
